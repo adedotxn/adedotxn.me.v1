c663c545350e6426306d77a519324a43f60ab2e6
Phyf3 <phade075@gmail.com>
2023-01-19 23:31:39 +0100

    minor update

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -11,7 +11,24 @@ export default function Layout({ children }: { children: ReactNode }) {
           name="description"
           content="Philip's Portfolio | Software Developer"
         />
-        <link rel="icon" href="https://fav.farm/🐱‍👤" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <Navbar />
       <main>{children} </main>
